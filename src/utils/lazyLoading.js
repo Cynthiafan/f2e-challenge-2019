95b452d285fddb8e3610400cef53668a6475359f
Cynthia <cynthiafan1230@gmail.com>
2019-07-30 18:03:36 +0800

    fix: Use npm instead

--- src/utils/lazyLoading.js
+++ src/utils/lazyLoading.js
@@ -1,7 +1,4 @@
 // ref. https://router.vuejs.org/zh/guide/advanced/lazy-loading.html
 
 export default (name, index = false) => () =>
-  import(
-    /* webpackChunkName: "page-" */
-    `../views/${name}${index ? '/index' : ''}.vue`
-  );
+  import(/* webpackChunkName: "page-" */ `../views/${name}${index ? '/index' : ''}.vue`);
